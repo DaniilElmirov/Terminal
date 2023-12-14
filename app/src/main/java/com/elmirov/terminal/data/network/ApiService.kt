a335e9e073c00f50c251ea6da6ad79914ae29efe
Ельмиров Даниил Владимирович <elmirovwork@gmail.com>
2023-12-14 23:37:57 +0700

    Изменил порядок получения свечей

--- app/src/main/java/com/elmirov/terminal/data/network/ApiService.kt
+++ app/src/main/java/com/elmirov/terminal/data/network/ApiService.kt
@@ -5,6 +5,6 @@ import retrofit2.http.GET
 
 interface ApiService {
 
-    @GET("aggs/ticker/AAPL/range/1/hour/2022-01-09/2023-01-09?adjusted=true&sort=asc&limit=50000&apiKey=jYdjt7h3fnLlrAD1ii00ZWtyQTg8zAh7")
+    @GET("aggs/ticker/AAPL/range/1/hour/2022-01-09/2023-01-09?adjusted=true&sort=desc&limit=50000&apiKey=jYdjt7h3fnLlrAD1ii00ZWtyQTg8zAh7")
     suspend fun getBars(): Result
 }
