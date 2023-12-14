89fee75ee9a17053dd400220782266b733384ebf
Ельмиров Даниил Владимирович <elmirovwork@gmail.com>
2023-12-14 21:02:37 +0700

    Добавил api service и factory

---
+++ app/src/main/java/com/elmirov/terminal/data/network/ApiService.kt
@@ -0,0 +1,10 @@
+package com.elmirov.terminal.data.network
+
+import com.elmirov.terminal.data.model.Result
+import retrofit2.http.GET
+
+interface ApiService {
+
+    @GET("aggs/ticker/AAPL/range/1/hour/2022-01-09/2023-01-09?adjusted=true&sort=asc&limit=50000&apiKey=jYdjt7h3fnLlrAD1ii00ZWtyQTg8zAh7")
+    suspend fun getBars(): Result
+}
