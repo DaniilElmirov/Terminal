61147964dc6bbf26d8dcc378e1764e4fe5ff91ad
Ельмиров Даниил Владимирович <elmirovwork@gmail.com>
2023-12-14 21:03:09 +0700

    Добавил view model

---
+++ app/src/main/java/com/elmirov/terminal/presentation/TerminalViewModel.kt
@@ -0,0 +1,35 @@
+package com.elmirov.terminal.presentation
+
+import android.util.Log
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.elmirov.terminal.data.network.ApiFactory
+import com.elmirov.terminal.presentation.TerminalScreenState.Content
+import com.elmirov.terminal.presentation.TerminalScreenState.Initial
+import kotlinx.coroutines.CoroutineExceptionHandler
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.launch
+
+class TerminalViewModel : ViewModel() {
+
+    private val apiService = ApiFactory.apiService
+
+    private val _state = MutableStateFlow<TerminalScreenState>(Initial)
+    val state = _state.asStateFlow()
+
+    private val exceptionHandler = CoroutineExceptionHandler { _, throwable ->
+        Log.d("TerminalViewModel", "Exception: $throwable")
+    }
+
+    init {
+        getBars()
+    }
+
+    private fun getBars() {
+        viewModelScope.launch(exceptionHandler) {
+            val bars = apiService.getBars().bars
+            _state.value = Content(bars = bars)
+        }
+    }
+}
