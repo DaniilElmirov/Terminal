fe6372c8f1e0c62171f8e87f3affb49a126b28af
Ельмиров Даниил Владимирович <elmirovwork@gmail.com>
2023-12-15 22:41:25 +0700

    Добавил скролл с помощью метода translate

--- app/src/main/java/com/elmirov/terminal/presentation/Terminal.kt
+++ app/src/main/java/com/elmirov/terminal/presentation/Terminal.kt
@@ -13,6 +13,7 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.drawscope.translate
 import com.elmirov.terminal.data.model.Bar
 import kotlin.math.roundToInt
 
@@ -27,9 +28,24 @@ fun Terminal(
         mutableStateOf(100)
     }
 
-    val transformableState = TransformableState { zoomChange, _, _ ->
+    var scrollBy by remember {
+        mutableStateOf(0f)
+    }
+
+    var barWidth by remember {
+        mutableStateOf(0f)
+    }
+
+    var terminalWidth by remember {
+        mutableStateOf(0f)
+    }
+
+    val transformableState = TransformableState { zoomChange, panChange, _ ->
         visibleBarCount = (visibleBarCount / zoomChange).roundToInt()
             .coerceIn(MIN_VISIBLE_BAR_COUNT, bars.size)
+
+        scrollBy = (scrollBy + panChange.x)
+            .coerceIn(0f, bars.size * barWidth - terminalWidth)
     }
 
     Canvas(
@@ -38,28 +54,31 @@ fun Terminal(
             .background(Color.Black)
             .transformable(transformableState),
     ) {
-        val barWidth = size.width / visibleBarCount
+        barWidth = size.width / visibleBarCount
+        terminalWidth = size.width
 
         val min = bars.minOf { it.low }
         val max = bars.maxOf { it.high }
         val pxPerPoint = size.height / (max - min)
 
-        bars.take(visibleBarCount).forEachIndexed { index, bar ->
-            val offsetX = size.width - index * barWidth
+        translate(left = scrollBy) {
+            bars.forEachIndexed { index, bar ->
+                val offsetX = size.width - index * barWidth
 
-            drawLine(
-                color = Color.White,
-                start = Offset(offsetX, size.height - ((bar.low - min) * pxPerPoint)),
-                end = Offset(offsetX, size.height - ((bar.high - min) * pxPerPoint)),
-                strokeWidth = 1f,
-            )
+                drawLine(
+                    color = Color.White,
+                    start = Offset(offsetX, size.height - ((bar.low - min) * pxPerPoint)),
+                    end = Offset(offsetX, size.height - ((bar.high - min) * pxPerPoint)),
+                    strokeWidth = 1f,
+                )
 
-            drawLine(
-                color = if (bar.open < bar.close) Color.Green else Color.Red,
-                start = Offset(offsetX, size.height - ((bar.open - min) * pxPerPoint)),
-                end = Offset(offsetX, size.height - ((bar.close - min) * pxPerPoint)),
-                strokeWidth = barWidth / 2,
-            )
+                drawLine(
+                    color = if (bar.open < bar.close) Color.Green else Color.Red,
+                    start = Offset(offsetX, size.height - ((bar.open - min) * pxPerPoint)),
+                    end = Offset(offsetX, size.height - ((bar.close - min) * pxPerPoint)),
+                    strokeWidth = barWidth / 2,
+                )
+            }
         }
     }
 }
