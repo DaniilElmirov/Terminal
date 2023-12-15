4e33df2f350205ce7dd0bc9466d38fbfe8518c7a
Ельмиров Даниил Владимирович <elmirovwork@gmail.com>
2023-12-15 23:37:13 +0700

    Способ при помощи своего Saver'а (1 параметр)

--- app/src/main/java/com/elmirov/terminal/presentation/Test.kt
+++ app/src/main/java/com/elmirov/terminal/presentation/Test.kt
@@ -1,22 +1,22 @@
 package com.elmirov.terminal.presentation
 
-import android.os.Parcelable
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.MutableState
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.saveable.Saver
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import kotlinx.parcelize.Parcelize
 
 @Composable
 fun Test() {
-    var testData by rememberSaveable {
+    var testData by rememberSaveable(saver = TestData.saver) {
         mutableStateOf(TestData(0))
     }
 
@@ -31,7 +31,17 @@ fun Test() {
     }
 }
 
-@Parcelize
 data class TestData(
     val number: Int,
-) : Parcelable
+) {
+    companion object {
+        val saver: Saver<MutableState<TestData>, Int> = Saver(
+            save = {
+                it.value.number
+            },
+            restore = {
+                mutableStateOf(TestData(number = it))
+            },
+        )
+    }
+}
