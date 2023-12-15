d083de6e7bac72961ecdd0a35e186a1e904673e1
Ельмиров Даниил Владимирович <elmirovwork@gmail.com>
2023-12-15 22:29:15 +0700

    Добавил отображение цены закрытия и открытия

--- app/src/main/java/com/elmirov/terminal/presentation/Terminal.kt
+++ app/src/main/java/com/elmirov/terminal/presentation/Terminal.kt
@@ -13,7 +13,6 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.unit.dp
 import com.elmirov.terminal.data.model.Bar
 import kotlin.math.roundToInt
 
@@ -47,11 +46,19 @@ fun Terminal(
 
         bars.take(visibleBarCount).forEachIndexed { index, bar ->
             val offsetX = size.width - index * barWidth
+
             drawLine(
                 color = Color.White,
                 start = Offset(offsetX, size.height - ((bar.low - min) * pxPerPoint)),
                 end = Offset(offsetX, size.height - ((bar.high - min) * pxPerPoint)),
-                strokeWidth = 1.dp.toPx(),
+                strokeWidth = 1f,
+            )
+
+            drawLine(
+                color = if (bar.open < bar.close) Color.Green else Color.Red,
+                start = Offset(offsetX, size.height - ((bar.open - min) * pxPerPoint)),
+                end = Offset(offsetX, size.height - ((bar.close - min) * pxPerPoint)),
+                strokeWidth = barWidth / 2,
             )
         }
     }
