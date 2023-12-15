838ab334d9b30af0ae7c30e49599f516aefa71d3
Ельмиров Даниил Владимирович <elmirovwork@gmail.com>
2023-12-15 23:28:40 +0700

    Способ сохранения объекта в стейте при помощи @Parcelize

--- app/src/main/java/com/elmirov/terminal/presentation/MainActivity.kt
+++ app/src/main/java/com/elmirov/terminal/presentation/MainActivity.kt
@@ -13,18 +13,19 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            TerminalTheme {
-                val viewModel: TerminalViewModel = viewModel()
-                val screenState = viewModel.state.collectAsState()
-
-                when (val currentState = screenState.value) {
-                    Initial -> Unit
-
-                    is Content -> {
-                        Terminal(bars = currentState.bars)
-                    }
-                }
-            }
+//            TerminalTheme {
+//                val viewModel: TerminalViewModel = viewModel()
+//                val screenState = viewModel.state.collectAsState()
+//
+//                when (val currentState = screenState.value) {
+//                    Initial -> Unit
+//
+//                    is Content -> {
+//                        Terminal(bars = currentState.bars)
+//                    }
+//                }
+//            }
+            Test()
         }
     }
 }
