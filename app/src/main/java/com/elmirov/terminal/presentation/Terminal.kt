958a4e46191765bd598242862a60d1d84ace7144
Ельмиров Даниил Владимирович <elmirovwork@gmail.com>
2023-12-17 16:56:46 +0700

    Добавил линии максимальной, минимальной и последней цен

--- app/src/main/java/com/elmirov/terminal/presentation/Terminal.kt
+++ app/src/main/java/com/elmirov/terminal/presentation/Terminal.kt
@@ -5,14 +5,18 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.gestures.TransformableState
 import androidx.compose.foundation.gestures.transformable
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.PathEffect
+import androidx.compose.ui.graphics.drawscope.DrawScope
 import androidx.compose.ui.graphics.drawscope.translate
 import androidx.compose.ui.layout.onSizeChanged
+import androidx.compose.ui.unit.dp
 import com.elmirov.terminal.data.model.Bar
 import kotlin.math.roundToInt
 
@@ -39,6 +43,10 @@ fun Terminal(
         modifier = Modifier
             .fillMaxSize()
             .background(Color.Black)
+            .padding(
+                top = 32.dp,
+                bottom = 32.dp,
+            )
             .transformable(transformableState)
             .onSizeChanged {
                 terminalState = terminalState.copy(terminalWidth = it.width.toFloat())
@@ -68,5 +76,54 @@ fun Terminal(
                 )
             }
         }
+
+        bars.firstOrNull()?.let {
+            drawPricesLine(
+                min = min,
+                pxPerPoint = pxPerPoint,
+                lastPrice = it.close,
+            )
+        }
     }
+}
+
+private fun DrawScope.drawPricesLine(
+    min: Float,
+    pxPerPoint: Float,
+    lastPrice: Float,
+) {
+    //max
+    drawDashLine(
+        start = Offset(0f, 0f),
+        end = Offset(size.width, 0f),
+    )
+
+    //last price
+    drawDashLine(
+        start = Offset(0f, size.height - ((lastPrice - min) * pxPerPoint)),
+        end = Offset(size.width, size.height - ((lastPrice - min) * pxPerPoint)),
+    )
+
+    //min
+    drawDashLine(
+        start = Offset(0f, size.height),
+        end = Offset(size.width, size.height),
+    )
+}
+
+private fun DrawScope.drawDashLine(
+    color: Color = Color.White,
+    start: Offset,
+    end: Offset,
+    strokeWidth: Float = 1f,
+) {
+    drawLine(
+        color = color,
+        start = start,
+        end = end,
+        strokeWidth = strokeWidth,
+        pathEffect = PathEffect.dashPathEffect(
+            intervals = floatArrayOf(4.dp.toPx(), 4.dp.toPx())
+        ),
+    )
 }
